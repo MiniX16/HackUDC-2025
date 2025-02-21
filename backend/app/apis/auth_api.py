25c5fe1b4a6ccb976427358c1063eb07f7db48c3
iago.castro.alvarez <iago.castro.alvarez@udc.es>
2025-02-22 00:35:55 +0100

    [Fix] Se corrige el entorno

--- backend/app/apis/auth_api.py
+++ backend/app/apis/auth_api.py
@@ -1,17 +1,15 @@
 import requests
+from requests.auth import HTTPBasicAuth
 from config import INDITEX_CLIENT_ID, INDITEX_CLIENT_SECRET, INDITEX_TOKEN_URL
 
 def get_auth_token():
-    """Obtiene el JWT Token desde la API de autenticación de Inditex"""
-    data = {
-        "grant_type": "client_credentials",
-        "client_id": INDITEX_CLIENT_ID,
-        "client_secret": INDITEX_CLIENT_SECRET
-    }
-
-    response = requests.post(INDITEX_TOKEN_URL, data=data)
+    response = requests.post(
+        INDITEX_TOKEN_URL,
+        auth=HTTPBasicAuth(INDITEX_CLIENT_ID, INDITEX_CLIENT_SECRET),  
+        data={"grant_type": "client_credentials", "scope": "technology.stock.read"}
+    )
 
     if response.status_code == 200:
-        return response.json().get("access_token")
-    else:
-        raise Exception(f"Error obteniendo el token: {response.status_code} - {response.text}")
+        return response.json()
+    
+    raise Exception(f"Error {response.status_code}: {response.text}")
--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,5 +1,5 @@
 from apis.auth_api import get_auth_token
 
 def generate_token():
-    """Llama a la API de autenticación y devuelve el token"""
-    return {"access_token": get_auth_token()}
+    """Llama a la API de autenticación y devuelve el token con su metadata"""
+    return get_auth_token()
