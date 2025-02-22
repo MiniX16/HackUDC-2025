db1188aa8f998a0aa5899ac3a3e93381c2f57eea
martindelrioalvarez <delrioalvarezmartin@gmail.com>
2025-02-22 16:01:45 +0100

    [FIX] Scroll Manual

--- frontend/src/pages/IntroPage.jsx
+++ frontend/src/pages/IntroPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import UploadImage from "../components/Uploadimage";
 import { getProducts } from "../services/api"; // Importamos la función
@@ -7,6 +7,7 @@ import "../styles.css"; // Importamos el CSS específico de esta página
 const IntroPage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [rowImages, setRowImages] = useState([[], [], []]); // Estado para almacenar imágenes de cada fila
+  const rowRefs = [useRef(null), useRef(null), useRef(null)];
 
   // Obtener imágenes del backend y actualizar cada fila
   useEffect(() => {
@@ -29,6 +30,13 @@ const IntroPage = () => {
     setSelectedImage(imageSrc);
   };
 
+  const handleScroll = (event, rowIndex) => {
+    if (rowRefs[rowIndex].current) {
+      event.preventDefault(); // Evita el scroll vertical al usar la rueda
+      rowRefs[rowIndex].current.scrollLeft += event.deltaY * 2; // Ajusta la velocidad del desplazamiento
+    }
+  };
+
   return (
     <div className="intro-container">
       <h1 className="logo">INDEEPTEX</h1>
@@ -36,7 +44,13 @@ const IntroPage = () => {
         {rowImages.map((images, rowIndex) => {
           const scrollDirection = rowIndex === 1 ? "left" : "right";
           return (
-            <div key={rowIndex} className={`image-row row-${rowIndex + 1}`} style={{ width: "100%", overflow: "hidden" }}>
+            <div
+              key={rowIndex}
+              className={`image-row row-${rowIndex + 1}`}
+              style={{ width: "100%", overflowX: "auto", display: "flex" }}
+              ref={rowRefs[rowIndex]}
+              onWheel={(event) => handleScroll(event, rowIndex)}
+            >
               <motion.div
                 className="image-track"
                 animate={{ x: scrollDirection === "left" ? ["0%", "-100%"] : ["-100%", "0%"] }}
