4a3efa7358cf12c461d6d776b83c9f2cdf23c66e
iago.castro.alvarez <iago.castro.alvarez@udc.es>
2025-02-23 04:32:16 +0100

    [Fix] Se escoge producto aleatorio

--- backend/app/services/recommendation_service.py
+++ backend/app/services/recommendation_service.py
@@ -2,24 +2,26 @@ from bd.models import Product
 from utils.color_utils import get_matching_colors
 from sqlalchemy import func
 from sqlalchemy.orm import Session
+import random
 
 def get_recommendations_by_id(db: Session, color, allowed_categories):
-    """Busca productos compatibles y selecciona solo una prenda por categoría desde la BD"""
-    base_color = color.upper()  # Convertimos el color base a mayúsculas
-    
+    """Busca productos compatibles y selecciona solo una prenda aleatoria por categoría desde la BD"""
+    base_color = color.upper()
     matching_colors = [color.upper() for color in get_matching_colors(base_color)]
 
-    # Obtener productos compatibles desde la BD (misma categoría excluida)
     compatible_products = db.query(Product).filter(
         func.upper(Product.color).in_(matching_colors),
         Product.category.in_(allowed_categories)
     ).all()
 
-    # Seleccionar solo una prenda por categoría
-    selected_recommendations = {}
+    # Organizar productos por categoría
+    category_products = {}
     for product in compatible_products:
-        category = product.category
-        if category not in selected_recommendations:
-            selected_recommendations[category] = product
+        category_products.setdefault(product.category, []).append(product)
 
-    return list(selected_recommendations.values())
+    # Seleccionar un producto aleatorio por categoría
+    selected_recommendations = [
+        random.choice(products) for products in category_products.values()
+    ]
+
+    return selected_recommendations
